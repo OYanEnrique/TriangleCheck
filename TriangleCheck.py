f44716fa4f7f8295306054134546bffaac4a9c7f
Yan Enrique <30851408+OYanEnrique@users.noreply.github.com>
2025-08-04 01:37:33 -0300

    feat: Classify triangle type after validation
    - Expands the program to classify the type of triangle (Equilateral, Isosceles, or Scalene) if the sides can form a valid triangle.
    - Adds a nested if/elif/else block to check the equality of the sides after the initial validation passes.

--- TriangleCheck.py
+++ TriangleCheck.py
@@ -4,5 +4,11 @@ r3=float(input('Enter a number: '))
 
 if r1+r2>r3 and r1+r3>r2 and r2+r3>r1:
 	print('It can form a triangle! ')
+	if r1== r2 == r3:
+		print('It will form an equilateral triangle')
+	elif r1 == r2 or r1 == r3 or r2 == r3:
+		print('It will form an isosceles triangle')
+	elif r1 != r2 != r3:
+		print('It will form an scalene triangle')
 else:
 	print('It can not form a triangle')
